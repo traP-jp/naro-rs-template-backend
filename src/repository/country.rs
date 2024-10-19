04a984aa4d28875c0380f30e3c9cb0238d6dd40c
kenken714 <kenkenkeiko043@gmail.com>
2024-10-19 11:51:20 +0900

    2-1/feat: add logger

--- src/repository/country.rs
+++ src/repository/country.rs
@@ -1,11 +1,14 @@
 #[derive(sqlx::FromRow, serde::Serialize, serde::Deserialize)]
-#[sqlx(rename_all = "PascalCase")]
 #[serde(rename_all = "camelCase")]
 pub struct City {
     #[sqlx(rename = "ID")]
     pub id: Option<i32>,
+    #[sqlx(rename = "Name")]
     pub name: String,
+    #[sqlx(rename = "CountryCode")]
     pub country_code: String,
+    #[sqlx(rename = "District")]
     pub district: String,
+    #[sqlx(rename = "Population")]
     pub population: i32,
 }
